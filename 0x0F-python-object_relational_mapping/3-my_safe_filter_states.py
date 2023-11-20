1e72f058f682fb16bb20e331010fce84a1038fbc
Patrick <pchipungu50@gmail.com>
2023-11-20 09:36:25 +0200

    mainCommit

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+    A script that lists all states from the database
+    Username, password and database names are given as user args
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    sql = """SELECT * FROM states
+          WHERE name = %s
+          ORDER BY id ASC"""
+
+    cursor.execute(sql, (sys.argv[4],))
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,4 @@
+# 0x0F-python-object_relational_mapping
+Author:Patrick
+pchipungu50@gmail.com
+
